264bb0c054d6d6156143f993d34c356eefb56708
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 16:11:22 +0530

    Update and rename reverse.java to Reverse3.java

---
+++ Reverse3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ public class Reverse3
+{
+public  static void main(String args[])
+{
+int no,rem,rev=0;
+  Scanner sc=new Scanner(System.in);
+  no=sc.nextInt();
+while(no!=0)
+{
+rem=no%10;
+rev=rev*10+rem;
+no=no/10;
+}
+System.out.println(rev);
+}
+}
